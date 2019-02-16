ac04d2813d0a6f093c9b3bda4a99f722284befab
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:07:40 +0100

    Add files via upload

---
+++ AdventCode/Day10/Star.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day10
+{
+    public class Star
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int VelX { get; set; }
+
+        public int VelY { get; set; }
+
+        public Star(string input)
+        {
+            var splits = input.Split('>');
+
+            string coordinates = splits[0].Split('<')[1];
+            X = int.Parse(coordinates.Split(',')[0].Replace(" ", ""));
+            Y = int.Parse(coordinates.Split(',')[1].Replace(" ", ""));
+
+            string velocity = splits[1].Split('<')[1];
+            VelX = int.Parse(velocity.Split(',')[0].Replace(" ", ""));
+            VelY = int.Parse(velocity.Split(',')[1].Replace(" ", ""));
+        }
+    }
+}
