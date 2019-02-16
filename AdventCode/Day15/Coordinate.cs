ceef2070b53680fd9b95716086246e5df0200c17
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:14:59 +0100

    Add files via upload

---
+++ AdventCode/Day15/Coordinate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day15
+{
+    public class Coordinate
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Distance { get; set; }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Coordinate;
+            if (other == null)
+                return false;
+            return other.X == X && other.Y == Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return 100 * X + Y;
+        }
+    }
+}
---
+++ AdventCode/Day15/Unit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day15
+{
+    public class Unit
+    {
+        private static int _idGen = 1;
+
+        public int Id { get; set; }
+
+        public Race Race { get; private set; }
+
+        public Coordinate Position { get; set; }
+
+        public int HitPoints { get; set; }
+
+        public int Attack { get; private set; }
+
+        public Unit(int x, int y, Race race)
+        {
+            Id = _idGen++;
+            HitPoints = 200;
+            Attack = 3;
+
+            Position = new Coordinate(x, y);
+            Race = race;
+        }
+    }
+
+    public enum Race
+    {
+        Elf,
+        Goblin
+    };
+}
