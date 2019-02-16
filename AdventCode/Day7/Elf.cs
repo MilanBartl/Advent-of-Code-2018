77503305a930a054486a6c41c69a3bfe96e21f6a
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:00:50 +0100

    Add files via upload

---
+++ AdventCode/Day7/Elf.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day7
+{
+    public class Elf
+    {
+        public int Id { get; private set; }
+
+        private int _timeToFinishTask = 0;
+
+        private Step _currentTask;
+
+        public bool IsIdle { get { return _timeToFinishTask == 0; } }
+
+        public Elf(int id)
+        {
+            Id = id;
+        }
+
+        public void AssignTask(Step task)
+        {
+            _currentTask = task;
+            _timeToFinishTask = task.RequiredTime;
+        }
+
+        public Step CompleteTask()
+        {
+            var completed = _currentTask;
+            _currentTask = null;
+            return completed;
+        }
+
+        public bool Work()
+        {
+            if (!IsIdle)
+            {
+                _timeToFinishTask--;
+                return IsIdle;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ AdventCode/Day7/Step.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day7
+{
+    public class Step
+    {
+        public char Id { get; set; }
+
+        public List<Step> Before { get; set; }
+
+        public List<Step> After { get; private set; }
+
+        public int RequiredTime {  get { return Id - 4; } }
+
+        public bool IsWorking { get; set; }
+
+        public bool IsProcessed { get; set; }
+
+        public Step(char id)
+        {
+            Id = id;
+            Before = new List<Step>();
+            After = new List<Step>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Step;
+            if (other == null)
+                return false;
+            else
+                return other.Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
