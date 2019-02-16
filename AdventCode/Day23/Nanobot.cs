91c6c0d68565f22c312480e66b5574220ddc89e8
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:26:29 +0100

    Add files via upload

---
+++ AdventCode/Day23/Nanobot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day23
+{
+    public class Nanobot
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+
+        public int Radius { get; set; }
+
+        public Nanobot(string input)
+        {
+            var splits = input.Split(new[] { ">," }, StringSplitOptions.RemoveEmptyEntries);
+            Radius = int.Parse(splits[1].Substring(3));
+
+            string coords = splits[0].Substring(5);
+            splits = coords.Split(',');
+            X = int.Parse(splits[0]);
+            Y = int.Parse(splits[1]);
+            Z = int.Parse(splits[2]);
+        }
+    }
+}
