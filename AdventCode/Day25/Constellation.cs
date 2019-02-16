0447b31f219a6b61e83b80f393e8ca2a90bcdb8b
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:29:47 +0100

    Add files via upload

---
+++ AdventCode/Day25/Constellation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day25
+{
+    public class Constellation
+    {
+        public List<Point> Points { get; set; }
+
+        public Constellation(IEnumerable<Point> points)
+        {
+            Points = new List<Point>();
+            Points.AddRange(points);
+            foreach (var point in points)
+            {
+                point.Constellation = this;
+            }
+        }
+
+        public void AddPoints(IEnumerable<Point> points)
+        {
+            Points.AddRange(points);
+            foreach (var point in points)
+            {
+                point.Constellation = this;
+            }
+        }
+    }
+}
---
+++ AdventCode/Day25/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day25
+{
+    public class Point
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+
+        public int T { get; set; }
+
+        public Constellation Constellation { get; set; }
+
+        public Point(string input)
+        {
+            var splits = input.Split(',');
+            X = int.Parse(splits[0]);
+            Y = int.Parse(splits[1]);
+            Z = int.Parse(splits[2]);
+            T = int.Parse(splits[3]);
+        }
+    }
+}
