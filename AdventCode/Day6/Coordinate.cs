56cb1c455e2e6193581db05e163674adc3e47302
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 09:58:13 +0100

    Add files via upload

---
+++ AdventCode/Day6/Coordinate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day6
+{
+    public class Coordinate
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int LocationId { get; set; }
+
+        public int MinDistance { get; set; }
+
+        public int DistanceSum { get; set; }
+
+        public bool IsShared { get; set; }
+
+        public Coordinate()
+        {
+            LocationId = 0;
+            MinDistance = int.MaxValue;
+            DistanceSum = 0;
+        }
+    }
+}
---
+++ AdventCode/Day6/Location.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day6
+{
+    public class Location
+    {
+        public int Id { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Size { get; set; }
+
+        public bool IsInfinite { get; set; }
+
+        public Location(int id, string input)
+        {
+            Id = id;
+            var splits = input.Split(',');
+            X = int.Parse(splits[0]);
+            Y = int.Parse(splits[1].Substring(1));
+        }
+    }
+}
