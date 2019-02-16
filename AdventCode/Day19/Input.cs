7e7908f528e0787cca2677d190cfcbfed99e1d4b
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:22:55 +0100

    Add files via upload

---
+++ AdventCode/Day19/Input.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day19
+{
+    public class Input
+    {
+        public string OperationName { get; set; }
+
+        public int RegA { get; set; }
+
+        public int RegB { get; set; }
+
+        public int RegC { get; set; }
+
+        public int[] Register { get; set; }
+
+        public Input(string input)
+        {
+            var splits = input.Split(' ');
+            OperationName = splits[0];
+            RegA = int.Parse(splits[1]);
+            RegB = int.Parse(splits[2]);
+            RegC = int.Parse(splits[3]);
+        }
+    }
+}
---
+++ AdventCode/Day19/Operation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day19
+{
+    public class Operation
+    {
+        public string Name { get; private set; }
+
+        public int Id { get; set; }
+
+        public Func<Input, int[]> Compute;
+
+        public Operation(string name)
+        {
+            Name = name;
+            Id = -1;
+        }
+    }
+}
