4ae4ee4dd74319d3732fd4d7ebea7348a697baab
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:21:16 +0100

    Add files via upload

---
+++ AdventCode/Day18/Acre.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day18
+{
+    public class Acre
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public AcreType Type { get; set; }
+
+        public AcreType NextType { get; set; }
+
+        public Acre(int x, int y, AcreType type)
+        {
+            X = x;
+            Y = y;
+            Type = type;
+        }
+
+        public void Change()
+        {
+            Type = NextType;
+        }
+    }
+
+    public enum AcreType
+    {
+        OpenGround,
+        Trees,
+        Lumberyard
+    }
+}
---
+++ AdventCode/Day18/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day18
+{
+    public static class Extensions
+    {
+        public static bool Contains<T>(this List<T> data, List<T> otherData)
+        {
+            var dataLength = data.Count();
+            var otherDataLength = otherData.Count();
+
+            if (dataLength < otherDataLength)
+                return false;
+
+            return Enumerable.Range(0, dataLength - otherDataLength + 1)
+                .Any(skip => data.Skip(skip).Take(otherDataLength).SequenceEqual(otherData));
+        }
+    }
+}
