0c8eb5b7a7ee95b4f7330d7fb9efd7dc557de2af
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 09:51:44 +0100

    Add files via upload

---
+++ AdventCode/Day3/Claim.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day3
+{
+    public class Claim
+    {
+        public int Id { get; private set; }
+
+        public Coordinate TopLeft { get; private set; }
+
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        public HashSet<Coordinate> Area { get; private set; }
+
+        public Claim(string input)
+        {
+            var splits = input.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            Id = int.Parse(splits[0].Substring(1));
+            TopLeft = new Coordinate(int.Parse(splits[2].Substring(0, splits[2].IndexOf(','))),
+                int.Parse(splits[2].Substring(splits[2].IndexOf(',') + 1, splits[2].Length - splits[2].IndexOf(',') - 2)));
+            Width = int.Parse(splits[3].Substring(0, splits[3].IndexOf('x')));
+            Height = int.Parse(splits[3].Substring(splits[3].IndexOf('x') + 1));
+
+            Area = new HashSet<Coordinate>();
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    Area.Add(new Coordinate(TopLeft.X + i, TopLeft.Y + j));
+                }
+            }
+        }
+    }
+}
---
+++ AdventCode/Day3/Coordinate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day3
+{
+    public class Coordinate
+    {
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Coordinate;
+            if (other == null)
+                return false;
+            return X == other.X && Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return X + (Y * 1000);
+        }
+    }
+}
