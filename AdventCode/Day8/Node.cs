ca1ee535c91672fa1c82ff02bc8c9bb8ae386b90
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:03:56 +0100

    Add files via upload

---
+++ AdventCode/Day8/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day8
+{
+    public class Node
+    {
+        public int Id { get; set; }
+
+        public List<Node> Children { get; set; }
+
+        public List<int> Metadata { get; set; }
+
+        public int References { get; set; }
+
+        public Node(int id)
+        {
+            Id = id;
+            References = 0;
+            Children = new List<Node>();
+            Metadata = new List<int>();
+        }
+    }
+}
