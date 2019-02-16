eb43d419057656f670652b2157ee0b4ee1386f35
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:25:04 +0100

    Add files via upload

---
+++ AdventCode/Day22/CaveRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day22
+{
+    public class CaveRoom
+    {
+        public Enums.CaveType Type { get; set; }
+
+        public int GeoIndex { get; set; }
+
+        public int ErosionLvl { get; set; }
+
+        public CaveRoom(Enums.CaveType type, int geoIndex, int erosion)
+        {
+            Type = type;
+            GeoIndex = geoIndex;
+            ErosionLvl = erosion;
+        }
+    }    
+}
---
+++ AdventCode/Day22/Enums.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day22
+{
+    public class Enums
+    {
+        public enum CaveType
+        {
+            Rocky,
+            Wet,
+            Narrow
+        }
+
+        public enum Tool
+        {
+            Torch = 0, 
+            Climbing = 1,
+            None = 2
+        }
+
+        public static Dictionary<CaveType, Tool[]> CaveToolMapping = new Dictionary<CaveType, Tool[]>
+        {
+            { CaveType.Rocky, new [] { Tool.Torch, Tool.Climbing } },
+            { CaveType.Wet, new [] { Tool.Climbing, Tool.None } },
+            { CaveType.Narrow, new [] { Tool.None, Tool.Torch } }
+        };
+    }
+}
