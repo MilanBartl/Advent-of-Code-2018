bfc3bac7d6500ba54320f612f17ead3b96418152
MilanBartl <47664992+MilanBartl@users.noreply.github.com>
2019-02-16 10:19:09 +0100

    Add files via upload

---
+++ AdventCode/Day17/Stream.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventCode.Day15;
+
+namespace AdventCode.Day17
+{
+    public class Stream
+    {
+        public static Material[,] Ground { get; set; }
+
+        public static int MaxX { get; set; }
+
+        public static int MaxY { get; set; }
+
+        public Coordinate StartPos { get; set; }
+
+        /// <summary>
+        /// Pointer to a current position of the current ~~~
+        /// </summary>
+        private Coordinate Pos { get; set; }
+
+        public Stream(int x, int y)
+        {
+            StartPos = new Coordinate(x, y);
+        }
+
+        public void Gush()
+        {
+            bool canFallDown = true;
+            bool canTrickleLeft = true;
+            bool canTrickleRight = true;
+            bool hasOverflown = false;
+
+            var childStreams = new List<Stream>();
+
+            Pos = StartPos;
+            Ground[Pos.X, Pos.Y] = Material.WaterWashed;
+
+            // fall down
+            while (canFallDown && !hasOverflown) 
+            {
+                Pos = new Coordinate(Pos.X, Pos.Y + 1);
+                Ground[Pos.X, Pos.Y] = Material.WaterWashed;
+
+                canFallDown = Pos.Y + 1 < MaxY && (Ground[Pos.X, Pos.Y + 1] != Material.Clay && Ground[Pos.X, Pos.Y + 1] != Material.Water);
+                hasOverflown = Pos.Y + 1 == MaxY;
+            }
+
+            if (hasOverflown)
+                return;
+
+            while (childStreams.Count == 0 && !hasOverflown)
+            {
+                var basinLevel = new List<Coordinate>();
+
+                // fill left
+                var basePos = Pos;
+                do
+                {
+                    basinLevel.Add(Pos);
+
+                    canTrickleLeft = Pos.X - 1 >= 0 && (Ground[Pos.X - 1, Pos.Y] != Material.Clay);
+                    canFallDown = Pos.Y + 1 < MaxY && (Ground[Pos.X, Pos.Y + 1] != Material.Clay && Ground[Pos.X, Pos.Y + 1] != Material.Water);
+
+                    Pos = new Coordinate(Pos.X - 1, Pos.Y);           
+                } while (canTrickleLeft && !canFallDown);
+
+                if (canFallDown)
+                    childStreams.Add(new Stream(Pos.X + 1, Pos.Y));
+
+                // fill right
+                Pos = basePos;
+                do
+                {
+                    basinLevel.Add(Pos);
+
+                    canTrickleRight = Pos.X + 1 < MaxX && (Ground[Pos.X + 1, Pos.Y] != Material.Clay);
+                    canFallDown = Pos.Y + 1 < MaxY && (Ground[Pos.X, Pos.Y + 1] != Material.Clay && Ground[Pos.X, Pos.Y + 1] != Material.Water);
+
+                    Pos = new Coordinate(Pos.X + 1, Pos.Y);     
+                } while (canTrickleRight && !canFallDown);
+
+                if (canFallDown)
+                    childStreams.Add(new Stream(Pos.X - 1, Pos.Y));
+
+                if (Pos.Y - 1 == 0)
+                    break;  // this should never happen
+
+                if (childStreams.Count == 0)
+                    basinLevel.ForEach(pos => Ground[pos.X, pos.Y] = Material.Water);
+                else
+                    basinLevel.ForEach(pos => Ground[pos.X, pos.Y] = Material.WaterWashed);
+
+                // move one level up
+                Pos = basePos;
+                Pos = new Coordinate(Pos.X, Pos.Y - 1);
+            }
+
+            foreach (var child in childStreams)
+            {
+                child.Gush();
+            }
+        }
+    }
+}
---
+++ AdventCode/Day17/Water.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode.Day17
+{
+    /// <summary>
+    /// Bad idea, it did leave some of the tanks half empty
+    /// </summary>
+    public class Water
+    {
+        private static int _countGen = 1;
+
+        private int _count = _countGen++;
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public bool CanFallDown { get; set; }
+
+        public bool CanMoveLeft { get; set; }
+
+        public bool CanMoveRight { get; set; }
+
+        public bool MovesLeft { get; set; }
+
+        public bool HasBounced { get; set; }
+
+        public bool HasTurnedAround { get; set; }
+
+        public Water(int x, int y, bool movesLeft)
+        {
+            X = x;
+            Y = y;
+            MovesLeft = movesLeft;
+        }
+
+        /// <summary>
+        /// Flows water through the ground
+        /// </summary>
+        /// <returns>True if water overflows</returns>
+        public bool Flow(Material[,] ground, int maxX, int maxY)
+        {
+            HasBounced = false;
+
+            CanFallDown = Y + 1 < maxY && (ground[X, Y + 1] != Material.Clay && ground[X, Y + 1] != Material.Water);
+            CanMoveLeft = X - 1 >= 0 && (ground[X - 1, Y] != Material.Clay && ground[X - 1, Y] != Material.Water);
+            CanMoveRight = X + 1 < maxX && (ground[X + 1, Y] != Material.Clay && ground[X + 1, Y] != Material.Water);
+
+            while (CanFallDown || CanMoveLeft || CanMoveRight)
+            {
+                if (HasBounced && !HasTurnedAround)
+                {
+                    var cloneSplash = new Water(X, Y, !MovesLeft);
+                    cloneSplash.Flow(ground, maxX, maxY);
+                }
+
+                if (MovesLeft)
+                    TrickleLeftways(ground, maxX, maxY);
+                else
+                    TrickleRightways(ground, maxX, maxY);
+
+
+                // trickle away
+                if (Y + 1 >= maxY)
+                {
+                    ground[X, Y] = Material.WaterWashed;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void TrickleLeftways(Material[,] ground, int maxX, int maxY)
+        {
+            // trickle down
+            if (CanFallDown)
+            {
+                HasBounced = false;
+                HasTurnedAround = false;
+                MoveDown(ground);
+
+                CanFallDown = Y + 1 < maxY && (ground[X, Y + 1] != Material.Clay && ground[X, Y + 1] != Material.Water);
+                CanMoveLeft = X - 1 >= 0 && (ground[X - 1, Y] != Material.Clay && ground[X - 1, Y] != Material.Water);
+                CanMoveRight = X + 1 < maxX && (ground[X + 1, Y] != Material.Clay && ground[X + 1, Y] != Material.Water);
+            }
+            // trickle leftwards
+            else if (CanMoveLeft && !HasTurnedAround)
+            {
+                if (CanMoveRight && ground[X - 1, Y] != Material.WaterWashed)
+                    HasBounced = true;
+                MoveLeft(ground);
+
+                CanFallDown = Y + 1 < maxY && (ground[X, Y + 1] != Material.Clay && ground[X, Y + 1] != Material.Water);
+                CanMoveLeft = X - 1 >= 0 && (ground[X - 1, Y] != Material.Clay && ground[X - 1, Y] != Material.Water);
+                CanMoveRight = X + 1 < maxX && (ground[X + 1, Y] != Material.Clay && ground[X + 1, Y] != Material.Water);
+            }
+            // trickle rightwards
+            else if (CanMoveRight)
+            {
+                HasTurnedAround = true;
+                MoveRight(ground);
+
+                CanFallDown = Y + 1 < maxY && (ground[X, Y + 1] != Material.Clay && ground[X, Y + 1] != Material.Water);
+                //CanMoveLeft = X - 1 >= 0 && (ground[X - 1, Y] != Material.Clay && ground[X - 1, Y] != Material.Water);
+                CanMoveRight = X + 1 < maxX && (ground[X + 1, Y] != Material.Clay && ground[X + 1, Y] != Material.Water);
+            }
+        }
+
+        private void TrickleRightways(Material[,] ground, int maxX, int maxY)
+        {
+            // trickle down
+            if (CanFallDown)
+            {
+                HasBounced = false;
+                HasTurnedAround = false;
+                MoveDown(ground);
+
+                CanFallDown = Y + 1 < maxY && (ground[X, Y + 1] != Material.Clay && ground[X, Y + 1] != Material.Water);
+                CanMoveLeft = X - 1 >= 0 && (ground[X - 1, Y] != Material.Clay && ground[X - 1, Y] != Material.Water);
+                CanMoveRight = X + 1 < maxX && (ground[X + 1, Y] != Material.Clay && ground[X + 1, Y] != Material.Water);
+            }
+            // trickle rightwards
+            else if (CanMoveRight && !HasTurnedAround)
+            {
+                if (CanMoveRight && ground[X + 1, Y] != Material.WaterWashed)
+                    HasBounced = true;
+                MoveRight(ground);
+
+                CanFallDown = Y + 1 < maxY && (ground[X, Y + 1] != Material.Clay && ground[X, Y + 1] != Material.Water);
+                CanMoveRight = X + 1 < maxX && (ground[X + 1, Y] != Material.Clay && ground[X + 1, Y] != Material.Water);
+                CanMoveLeft = X - 1 >= 0 && (ground[X - 1, Y] != Material.Clay && ground[X - 1, Y] != Material.Water);
+            }
+            // trickle leftwards
+            else if (CanMoveLeft)
+            {
+                HasTurnedAround = true;
+                MoveLeft(ground);
+
+                CanFallDown = Y + 1 < maxY && (ground[X, Y + 1] != Material.Clay && ground[X, Y + 1] != Material.Water);
+                CanMoveLeft = X - 1 >= 0 && (ground[X - 1, Y] != Material.Clay && ground[X - 1, Y] != Material.Water);
+            }
+        }
+
+        private void MoveDown(Material[,] ground)
+        {
+            ground[X, Y] = Material.WaterWashed;
+            Y++;
+            ground[X, Y] = Material.Water;
+        }
+
+        private void MoveLeft(Material[,] ground)
+        {
+            ground[X, Y] = Material.WaterWashed;
+            X--;
+            ground[X, Y] = Material.Water;
+        }
+
+        private void MoveRight(Material[,] ground)
+        {
+            ground[X, Y] = Material.WaterWashed;
+            X++;
+            ground[X, Y] = Material.Water;
+        }
+    }
+}
